2ba73a2602993ec42ede812dcb5af4fc05a83879
Rizka Sanggar Darmawan <angga2oioi@gmail.com>
2025-02-19 08:54:17 +0700

    change create project endpoint

--- src/app/api/v1/accounts/me/projects/route.js
+++ src/app/api/v1/accounts/me/projects/route.js
@@ -1,11 +1,12 @@
 //@ts-check
 
-import { MANAGE_ACCOUNT_ROLES, NO_ACCESS_ERR_CODE, NO_ACCESS_ERR_MESSAGE, SUCCESS_ERR_CODE, SUCCESS_ERR_MESSAGE } from "@/global/utils/constant";
+import { MANAGE_PROJECT_ROLES, NO_ACCESS_ERR_CODE, NO_ACCESS_ERR_MESSAGE, SUCCESS_ERR_CODE, SUCCESS_ERR_MESSAGE } from "@/global/utils/constant";
 import { HttpError, parseError } from "@/global/utils/functions";
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { validateCookies } from "@/server/module/auth/auth.service";
-import { paginateProject } from "@/server/module/project/project.service";
+import { createProject, paginateProject } from "@/server/module/project/project.service";
+import { canIManage } from "@/server/module/account/account.service";
 
 export async function GET(request, { params }) {
     try {
@@ -26,6 +27,40 @@ export async function GET(request, { params }) {
         } = Object.fromEntries(searchParams.entries())
 
         let data = await paginateProject({ search, account: account?.id }, sortBy, limit, page)
+
+        return NextResponse.json({
+            error: SUCCESS_ERR_CODE,
+            message: SUCCESS_ERR_MESSAGE,
+            data
+        });
+
+
+    } catch (e) {
+        return NextResponse.json(parseError(e), { status: e?.error || 400 });
+    }
+}
+
+export async function POST(request, { params }) {
+    try {
+
+        const { account, token } = await validateCookies(cookies);
+        if (!token) {
+            throw HttpError(NO_ACCESS_ERR_CODE, NO_ACCESS_ERR_MESSAGE);
+        }
+
+        const canManage = await canIManage(account?.id, MANAGE_PROJECT_ROLES)
+        if (!canManage) {
+            throw HttpError(NO_ACCESS_ERR_CODE, NO_ACCESS_ERR_MESSAGE)
+        }
+
+        const body = await request.json();
+        let data = await createProject({
+            ...body,
+            accounts: [
+                ...(body?.accounts || []),
+                account?.id
+            ]
+        })
 
         return NextResponse.json({
             error: SUCCESS_ERR_CODE,
